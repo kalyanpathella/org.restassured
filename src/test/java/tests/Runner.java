ef1762dadeb992517fcc617cbc5dd4096b01a3eb
kalyanpathella <kalyanchakravarthi53@gmail.com>
2023-07-03 11:30:24 +0530

    pushed runner class

--- src/test/java/tests/Runner.java
+++ src/test/java/tests/Runner.java
@@ -1,4 +1,14 @@
 package tests;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-public class Runner {
+//For smoke testing(run @smoketest tagged scenarios only to check basic functionality)
+@CucumberOptions(features={"src\\test\\resources\\features\\feature1.feature"},
+                 glue={"gluecode"},
+                 monochrome=true,
+       plugin={"pretty","json:target/cucumber-reports.json",
+    		   "rerun:target\\failedsmoketests.txt"})
+public class Runner extends AbstractTestNGCucumberTests
+{
+	
 }
